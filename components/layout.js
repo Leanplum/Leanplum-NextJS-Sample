376fe4e18e85abb06367ac33ce20a9befa2d8cf1
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2022-03-10 22:09:23 +0000

    feat: multi-page website, apply branding

---
+++ components/layout.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Layout.module.css'
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Leanplum + Next.js</title>
+        <meta name="description" content="Sample app showing how to integrate the Leanplum JS SDK in a Next.js app" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <header className={styles.header}>
+        <nav className={styles.menu}>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+        </nav>
+      </header>
+
+      <main className={styles.main}>
+        {children}
+      </main>
+
+      <footer className={styles.footer}>
+        Leanplum + Next.js demo application
+      </footer>
+    </div>
+  )
+}
---
+++ pages/products/index.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/Products.module.css'
+
+export default function Products() {
+  return (
+    <div className={styles.container}>
+      Product listing
+    </div>
+  )
+}
+
